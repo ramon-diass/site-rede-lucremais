9cef1ecf6815e74a4a26694a2af116904c2fd938
Ramon Dias <ramondias2605@gmail.com>
2025-07-14 22:42:58 -0300

    atualização Carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,8 +1,47 @@
-const imagens = ["imagem1.png", "imagem2.png", "imagem3.png", "imagem4.png"];
+const imagens = ["imagem1.png", "imagem2.png", "imagem3.png"];
 let indiceAtual = 0;
 const imgElement = document.getElementById("imagem");
+const btnAnterior = document.getElementById("anterior");
+const btnProximo = document.getElementById("proximo");
 
-setInterval(() => {
-  indiceAtual = (indiceAtual + 1) % imagens.length;
+function atualizarImagem() {
   imgElement.src = "imagens/" + imagens[indiceAtual];
-}, 3000); // troca a cada 3 segundos
+}
+
+function proximaImagem() {
+  indiceAtual = (indiceAtual + 1) % imagens.length;
+  atualizarImagem();
+}
+
+function imagemAnterior() {
+  indiceAtual = (indiceAtual - 1 + imagens.length) % imagens.length;
+  atualizarImagem();
+}
+
+// Rotação automática
+let intervalo = setInterval(proximaImagem, 3000);
+
+// Pausar rotação ao passar o mouse
+imgElement.addEventListener("mouseenter", () => {
+  clearInterval(intervalo);
+});
+
+imgElement.addEventListener("mouseleave", () => {
+  intervalo = setInterval(proximaImagem, 3000);
+});
+
+// Controle manual
+btnProximo.addEventListener("click", () => {
+  proximaImagem();
+  reiniciarIntervalo();
+});
+
+btnAnterior.addEventListener("click", () => {
+  imagemAnterior();
+  reiniciarIntervalo();
+});
+
+function reiniciarIntervalo() {
+  clearInterval(intervalo);
+  intervalo = setInterval(proximaImagem, 3000); // reinicia temporizador
+}
